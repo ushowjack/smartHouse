011a5e1ec292d515bcbc9c96e7e2a123c9342444
ushow <441332608@qq.com>
2017-04-22 18:21:50 +0800

    更换错误的文件

--- script/components/tableComponent.js
+++ script/components/tableComponent.js
@@ -1,2 +1,57 @@
-"use strict";var table=[[{name:"gfawt",sort:!0},{name:"gfawt",sort:!0},{name:"gfawt",sort:!1}],["gfawt","fewfgw","gwgw"],["gfawt","fewfgw","gwgw"],["gfawt","fewfgw","gwgw"]],tableComponent=new Vue({el:".table",template:'\n        <table class="table">\n            <template v-for="(item,index) in table">\n                <thead v-if="index === 0">\n                    <tr>\n                        <th>#</th>\n                        <th v-for="(childrenItem,index) in item">\n                            {{childrenItem.name}}\n                            <span v-if="childrenItem.sort" @click="sortBtn">/</span>\n                        </th>\n                    </tr>\n                </thead>\n                <tbody v-else>\n                    <tr>\n                        <td>{{index}}</td>\n                        <td v-for="(childrenItem,index) in item">{{childrenItem}}</td>\n                    </tr>\n                </tbody>\n            </template>\n        </table>\n    ',data:{table:table},methods:{sortBtn:function(){alert("This is sort!")}}});
-//# sourceMappingURL=tableComponent.js.map
+/**
+ * Created with ES6.
+ * User: Ushow Jack/youshoujie@tomee.cn
+ * Date: 2017/4/21
+ * Time: 11:43
+ */
+"use strict"
+/**
+ * data =  [
+ *    [{val:1,sort:false},{val:1,sort:true},{val:1,sort:true},{val:1,sort:true}],
+ *    [1,2,3,4],
+ *    [1,2,3,4],
+ *    [1,2,3,4],
+ * ]
+ * @param data
+ */
+
+let table = [
+    [{name:"gfawt",sort:true},{name:"gfawt",sort:true},{name:"gfawt",sort:false}],
+    ["gfawt","fewfgw","gwgw"],
+    ["gfawt","fewfgw","gwgw"],
+    ["gfawt","fewfgw","gwgw"],
+];
+const tableComponent = new Vue({
+    el: '.table',
+    template: `
+        <table class="table">
+            <template v-for="(item,index) in table">
+                <thead v-if="index === 0">
+                    <tr>
+                        <th>#</th>
+                        <th v-for="(childrenItem,index) in item">
+                            {{childrenItem.name}}
+                            <span v-if="childrenItem.sort" @click="sortBtn">/</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody v-else>
+                    <tr>
+                        <td>{{index}}</td>
+                        <td v-for="(childrenItem,index) in item">{{childrenItem}}</td>
+                    </tr>
+                </tbody>
+            </template>
+        </table>
+    `,
+    data: {
+        'table': table
+    },
+    methods: {
+        sortBtn(){
+            alert("This is sort!")
+        }
+    }
+
+});
+
